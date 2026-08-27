82c5310f3db5f82111eb74e68f8aca10c4464503
Bharathraj v <bharathkavi878@gmail.com>
2026-08-27 16:11:40 +0530

    Use DATABASE_URL for deployment

--- src/db/connect.ts
+++ src/db/connect.ts
@@ -1,9 +1,5 @@
 import { Pool } from "pg";
 
 export const pool = new Pool({
-    user: "postgres",
-    host: "localhost",
-    database: "lead_query_db",
-    password: "Sadhana@30",
-    port: 5432,
+    connectionString: process.env.DATABASE_URL,
 });
